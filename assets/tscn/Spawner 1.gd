45bf3dd87d52c72053c4feab892df35cd09b932e
hightyrol <JonathanLochridge@letu.edu>
2024-03-06 21:52:58 -0600

    Not working 2
    Moved the non functional code to new function
    -Added stuff to allow triggering spawner with spacebar.

--- assets/tscn/Spawner 1.gd
+++ assets/tscn/Spawner 1.gd
@@ -3,11 +3,17 @@ extends MeshInstance3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pass
+
+func spawn(): #presumably this never runs because the if statement need to call this has an error that seems syntx related?
 	var PawnEnemy = load("res://assets/tscn/pawn.tscn") # Replace with function body.
 	var PawnCopies = PawnEnemy.instantiate()
 	PawnCopies._ready() #Runs with this but nothing actually happens.
 	add_child(PawnCopies) #This is when it branches off and gives the error.
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
+	IF iNPUT.is_action_pressed("spawn_unit"): #No idea why this is giving an error?
+		spawn()#Where is the missing tab?
 	pass
